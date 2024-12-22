ed80c2af107336f5a6d98d6c3662f1cb403cf81f
CarlavierVN <namto10042004@gmail.com>
2024-12-22 09:57:19 +0700

    add chatbot display style

--- ui.py
+++ ui.py
@@ -1,10 +1,15 @@
 import streamlit as st
+import time
 
 if "my_text" not in st.session_state:
     st.session_state.my_text = []
 
+def append_chat(str):
+    st.session_state.my_text.append(str)
+
 def submit():
     st.session_state.my_text.append(st.session_state.widget)
+    append_chat("Them chat cua ai o day alo!!!")
     st.session_state.widget = ""
 
 def clear():
@@ -13,10 +18,21 @@ def clear():
 st.button("Clear history", on_click=clear)
 st.text_input("", key='widget', on_change=submit, placeholder='Say something!!')
 
-user_chats = st.session_state.my_text
+chat_history = st.session_state.my_text
 
 chat_entity = ["User:", "Chatbot:"]
 
-for i in range(len(user_chats) - 1, -1, -1):
+if len(chat_history) > 0:
+    st.write(chat_entity[(len(chat_history) - 1) % 2])
+    display_chat = ""
+    chatbot_chat = st.info(display_chat)
+
+for i in range(len(chat_history) - 2, -1, -1):
     st.write(chat_entity[i % 2])
-    st.info(user_chats[i])
+    st.info(chat_history[i])
+
+if len(chat_history) > 0:
+    for char in chat_history[-1]:
+        display_chat += char
+        chatbot_chat.info(display_chat)
+        time.sleep(0.05)
